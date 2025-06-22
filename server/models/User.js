455e9fda8401a9ea140cf5d73600ad351a3a9aff
jimztercrack <jimztercrack@yahoo.com>
2025-06-22 13:32:53 -0600

    Add files via upload

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,8 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs'); // ✅ USA bcryptjs, compatible con Render
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  firstName: String,
-  lastName: String,
-  role: {
-    type: String,
-    enum: ['admin', 'editor', 'viewer'],
-    default: 'viewer'
-  }
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-// Middleware: hashea la contraseña antes de guardar
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
-});
-
-// Método para comparar contraseñas
-UserSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
