f8e0c57c93f1d07fe2f398d8bfd94d322ee9b023
jimztercrack <jimztercrack@yahoo.com>
2025-06-27 12:42:56 -0600

    Add files via upload

--- client/src/switch.js
+++ client/src/switch.js
@@ -1,12 +1,3 @@
-const useLocalUrl = false;
-
-//const switch_url = 'http://localhost:3001';
-
-
-const switch_url = useLocalUrl 
-  ? 'https://trecenoticias-app-test-6d9a6dafed1f.herokuapp.com' 
-  : 'https://trecenoticias-app-80268873ff71.herokuapp.com';
-
+const switch_url = 'https://gestor-noticias-api.onrender.com';
 
 export default switch_url;
-
