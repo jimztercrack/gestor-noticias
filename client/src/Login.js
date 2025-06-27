eee0ef78c89cc93039a7ac078cdc156877282ce2
jimztercrack <jimztercrack@yahoo.com>
2025-06-27 11:39:43 -0600

    Add files via upload

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,58 +1,94 @@
-// Login.js
 import React, { useState } from 'react';
+import axios from 'axios';
+import { Button, TextField, Typography, Container, Box, CircularProgress } from '@mui/material';
+import logo from './utils/Logo-Trece.png';
+import switch_url from './switch';
 
-function Login() {
+const Login = ({ onLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      const response = await fetch("https://gestor-noticias-api.onrender.com/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ username, password })
-      });
-
-      const data = await response.json();
-      if (response.ok) {
-        localStorage.setItem("token", data.token);
-        window.location.href = "/dashboard";
+      const response = await axios.post(`${switch_url}/api/auth/login`, 
+        { username, password },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          }
+        });
+      onLogin(response.data.token, response.data.user);
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
       } else {
-        setError(data.message || "Error en el inicio de sesión");
+        setError('Error en el inicio de sesión');
       }
-    } catch (err) {
-      console.error("Error en la solicitud:", err);
-      setError("No se pudo conectar con el servidor.");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>Iniciar Sesión</h2>
-      {error && <p className="error">{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Entrar</button>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <img src={logo} alt="Logo" style={{ marginBottom: 20, width: '50%' }} />
+        <Typography component="h1" variant="h5" sx={{ color: '#d10000' }}>
+          Inicio de Sesión
+        </Typography>
+        {error && <Typography color="error">{error}</Typography>}
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Usuario"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Contraseña"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor: '#d10000', ':hover': { backgroundColor: 'darkred' } }}
+            disabled={loading}
+          >
+            {loading ? <CircularProgress size={24} /> : 'Iniciar Sesión'}
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default Login;
