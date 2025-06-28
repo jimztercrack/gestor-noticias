f09da8b3722c1b06309b13a1f5c5edeeb2258e20
jimztercrack <jimztercrack@yahoo.com>
2025-06-28 17:09:32 -0600

    Add files via upload

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,13 +15,22 @@ router.post('/register', async (req, res) => {
       return res.status(400).json({ message: 'El usuario ya existe' });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword, firstName, lastName });
+    // Ya no se encripta aquí, se hace en el pre('save') del modelo
+    const newUser = new User({ username, password, firstName, lastName });
     await newUser.save();
 
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    res.status(201).json({ token, user: { _id: newUser._id, username, firstName, lastName, role: newUser.role } });
+    res.status(201).json({
+      token,
+      user: {
+        _id: newUser._id,
+        username,
+        firstName,
+        lastName,
+        role: newUser.role
+      }
+    });
   } catch (err) {
     console.error('Error in register route:', err);
     res.status(500).json({ message: err.message });
@@ -45,12 +54,18 @@ router.post('/login', async (req, res) => {
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    // Verifica el contenido del objeto user
-    console.log('Usuario encontrado:', user);
-
     const { firstName, lastName } = user;
 
-    res.json({ token, user: { _id: user._id, username: user.username, firstName, lastName, role: user.role } });
+    res.json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        firstName,
+        lastName,
+        role: user.role
+      }
+    });
   } catch (err) {
     console.error('Error in login route:', err);
     res.status(500).json({ message: err.message });
